e28a101f2f07e823546fcabf12ac12cb32ea13c3
Benjamin Clewer <benjiclewer@gmail.com>
2021-11-25 13:54:12 +1030

    initial files, database create, db tables

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS company_employees_db;
+CREATE DATABASE company_employees_db;
+
+USE company_employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL,
+    department_id INT, 
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    manager_id INT, 
+    -- IF role(title) = manager FOREIGN KEY 
+)
