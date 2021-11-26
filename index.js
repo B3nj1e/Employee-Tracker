1f205a9dfc1384013d4f43836c80467c81e1a5ff
Benjamin Clewer <benjiclewer@gmail.com>
2021-11-26 11:07:50 +1030

    handling inquirer promises and setting up questions

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-const questions = inquirer 
+// need to import departments, roles and employee files
+
+const questions = () => inquirer 
   .prompt([
     {
       type: "list",
       name: "initial", 
       message: "Please select the option you would like to perform",
-      choices: "['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']"
+      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
     }
   ])
+
+  questions()
+
+  if (questions.choices === 'View all departments') {
+    // console.table(deparments)
+  };
+
+  if (questions.choices === 'View all roles') {
+
+  };
+
+  if (questions.choices === 'View all employees') {
+
+  }
+
+  if (questions.choices === 'Add a department') {
+
+  }
+
+  if (questions.choices === 'Add a role') {
+
+  }
+
+  if (questions.choices === 'Add an employee') {
+    
+  }
