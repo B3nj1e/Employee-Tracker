6496b2d9f293d8440d237c4a1fc91d0a5544015f
Benjamin Clewer <benjiclewer@gmail.com>
2021-11-26 10:48:00 +1030

    solved schema and seeds bugs

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,33 @@
-DROP DATABASE IF EXISTS company_employees_db;
-CREATE DATABASE company_employees_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
-USE company_employees_db;
+USE company_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL UNIQUE
 );
 
-CREATE TABLE employee_role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL, 
-    salary DECIMAL,
-    department_id INT, 
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
 );
 
-SELECT employee_role.department_id
-FROM employee_role
-JOIN department ON employee_role.department_id = department.id;
-
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL, 
-    last_name VARCHAR(30) NOT NULL, 
-    role_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id),
-    manager_id INT, 
+CREATE TABLE employees(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
+  ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,16 @@
+USE company_db;
+
 INSERT INTO department (name)
 VALUES ("IT"), 
         ("Legal"),
         ("Accounting");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("software engineer", 100000),
-        ("web developer", 100000),
-        ("lawyer", 120000), 
-        ("accountant", 80000);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "software engineer", 100000, 1),
+        (2, "web developer", 100000, 1),
+        (3, "lawyer", 120000, 2), 
+        (4, "accountant", 80000, 3);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("Ben", "Clewer"),
-        ("John", "Smith");
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Ben", "Clewer", 2, 2),
+        (2, "John", "Smith", 3, null);
